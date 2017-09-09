a6267f5fb28842c7b7fc0ac2c33c3037b6542c05
Isaac Sahle <isaac_sahle@hotmail.com>
2017-09-09 12:49:24 -0700

    PMan Skeleton

--- ProcessManager/rsi.c
+++ ProcessManager/rsi.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "rsi.h"
 
+void verifyCommand(char * command);
 
 int main(){
+char * command = NULL;
+//readline allocates memory with malloc, remember to free
+command = readline(PMAN_PROMPT);
 
-    char *input = NULL ;
-    char *prompt = "your command:";
-
-    input = readline(prompt);
-
-    printf("%s\n", input);
-    return 1;
+while(TRUE){
+  verifyCommand(command);
+  free(command);
+  command = readline(PMAN_PROMPT);
 }
 
+return 1;
+}
+
+void verifyCommand(char * command){
+//identify command
+char * token = strtok(command,COMMAND_DELIMITER);
+
+if(strcmp(token,BG) == 0){
+  printf("%s\n",token); 
+}else if(strcmp(token,BGLIST) == 0){
+  printf("%s\n",token); 
+}else if(strcmp(token,BGKILL) == 0){
+  printf("%s\n",token); 
+}else if(strcmp(token,BGSTOP) == 0){
+  printf("%s\n",token); 
+}else if(strcmp(token,BGSTART) == 0){
+  printf("%s\n",token);
+}else if(strcmp(token,PSTAT) == 0){
+  printf("%s\n",token); 
+}else{ 
+  printf("%s\n",UNRECOGNIZED_COMMAND); 
+}
+
+}
---
+++ ProcessManager/rsi.h
@@ -0,0 +1,14 @@
+#ifndef RSI_HEADER_FILE
+#define RSI_HEADER_FILE
+#define TRUE 1
+#define PMAN_PROMPT "PMan: > "
+#define COMMAND_DELIMITER " "
+#define UNRECOGNIZED_COMMAND "command not found"
+#define BG "bg"
+#define BGLIST "bglist"
+#define BGKILL "bgkill"
+#define BGSTOP "bgstop"
+#define BGSTART "bgstart" 
+#define PSTAT "pstat"
+
+#endif
